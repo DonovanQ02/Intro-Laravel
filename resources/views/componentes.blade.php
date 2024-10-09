a8acae8fa6f22c40c992b50119ca1f08545fde4c
DonovanQ02 <166979466+DonovanQ02@users.noreply.github.com>
2024-10-08 19:03:54 -0600

    practica 3.1
    finalizada

--- resources/views/componentes.blade.php
+++ resources/views/componentes.blade.php
@@ -4,4 +4,15 @@
 
 @section('contenido')
 
+    <x-Card encabezado="componente" titulo="Dinamico" textoBoton="Enviar"> hola amigos de youtube, como estan? sean bienvenidos al componente de esta tarjeta</x-Card>
+    
+    <x-Card encabezado="componente2" titulo="Dinamico2" textoBoton="No enviar" > hola amigos de youtube, como estan? sean bienvenidos al componente de esta tarjeta</x-Card>
+
+    <x-Alert tipo="primary">Holaaaaa, soy azul</x-Alert>
+    <x-Alert tipo="secondary">Holaaaa, soy gris</x-Alert>
+    <x-Alert tipo="danger">Ay, me enchile y soy rojo</x-Alert>
+
+
+
+
 @endsection
